9f0bf733a9ab755427bd96caf2c2f087c0914d3c
Vladyslav Komar <valdek125@gmail.com>
2022-05-26 02:22:42 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function htmlImg({ preview, original, description }) {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href=${original} onclick="return false;">
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`
+}
+    
+const htmlImgs = (Imgs) => Imgs.reduce((acc, img) => acc + htmlImg(img), "")
+
+const gallery = document.querySelector(".gallery")
+
+gallery.innerHTML += htmlImgs(galleryItems)
+
+gallery.addEventListener("click", (e) => {
+    const img = e.target
+
+    const instance = basicLightbox.create(`<img src=${img.dataset.source}>`)
+
+    instance.show()
+})
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        const modal = document.querySelector(".basicLightbox")
+        modal.remove()
+    }
+}
+)
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+function htmlImg({ preview, original, description }) {
+    return `<li>
+  <a class="gallery__item" href=${original} onclick="return false;">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`
+}
+
+const htmlImgs = (Imgs) => Imgs.reduce((acc, img) => acc + htmlImg(img), "")
+
+const gallery = document.querySelector(".gallery")
+
+gallery.innerHTML += htmlImgs(galleryItems)
+
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captions:true,captionPosition:'bottom',captionSelector:'img',
+   captionType: 'atrr', captionsData:"alt",captionDelay:250,})
+
